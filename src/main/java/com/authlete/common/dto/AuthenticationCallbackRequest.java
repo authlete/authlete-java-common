56428054f301b1d3b977346f88b1b539075d4d08
Takahiko Kawasaki <taka@authlete.com>
2014-11-17 17:44:33 +0900

    Added AuthenticationCallbackRequest and AuthenticationCallbackResponse.

---
+++ src/main/java/com/authlete/common/dto/AuthenticationCallbackRequest.java
@@ -0,0 +1,253 @@
+/*
+ * Copyright (C) 2014 Authlete, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.authlete.common.dto;
+
+
+import java.io.Serializable;
+
+
+/**
+ * Authentication request from Authlete to a service implementation.
+ *
+ * @author Takahiko Kawasaki
+ */
+public class AuthenticationCallbackRequest implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+
+    private long apiKey;
+    private String id;
+    private String password;
+    private String[] claims;
+    private String[] claimsLocales;
+
+
+    /**
+     * Get the API key of the target service.
+     *
+     * @return
+     *         The API key of the target service.
+     */
+    public long getApiKey()
+    {
+        return apiKey;
+    }
+
+
+    /**
+     * Set the API key of the target service.
+     *
+     * @param apiKey
+     *         The API key of the target service.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackRequest setApiKey(long apiKey)
+    {
+        this.apiKey = apiKey;
+
+        return this;
+    }
+
+
+    /**
+     * Get the ID of the end-user to authenticate.
+     *
+     * <p>
+     * This property holds the value of the login ID that the end-user
+     * has entered to the login ID field in the UI of the <a href=
+     * "https://tools.ietf.org/html/rfc6749#section-3.1">authorization
+     * endpoint</a>, or the value of {@code username} request parameter
+     * to the <a href="https://tools.ietf.org/html/rfc6749#section-3.2"
+     * >token endpoint</a> in the case of <a href=
+     * "https://tools.ietf.org/html/rfc6749#section-4.3">Resource
+     * Owner Password Credentials</a> flow.
+     * </p>
+     *
+     * <p>
+     * This property is always set when Authlete makes a request.
+     * </p>
+     *
+     * @return
+     *         The ID of the end-user to authenticate.
+     */
+    public String getId()
+    {
+        return id;
+    }
+
+
+    /**
+     * Set the ID of the end-user to authenticate.
+     *
+     * @param id
+     *         The ID of the end-user to authenticate.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackRequest setId(String id)
+    {
+        this.id = id;
+
+        return this;
+    }
+
+
+    /**
+     * Get the password of the end-user to authenticate.
+     *
+     * <p>
+     * This property holds the value of the password that the end-user
+     * has entered to the password field in the UI of the <a href=
+     * "https://tools.ietf.org/html/rfc6749#section-3.1">authorization
+     * endpoint</a>, or the value of {@code password} request parameter
+     * to the <a href="https://tools.ietf.org/html/rfc6749#section-3.2"
+     * >token endpoint</a> in the case of <a href=
+     * "https://tools.ietf.org/html/rfc6749#section-4.3">Resource
+     * Owner Password Credentials</a> flow.
+     * </p>
+     *
+     * <p>
+     * This property is always set when Authlete makes a request.
+     * </p>
+     *
+     * @return
+     *         The password of the end-user to authenticate.
+     */
+    public String getPassword()
+    {
+        return password;
+    }
+
+
+    /**
+     * Set the password of the end-user to authenticate.
+     *
+     * @param password
+     *         The password of the end-user to authenticate.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackRequest setPassword(String password)
+    {
+        this.password = password;
+
+        return this;
+    }
+
+
+    /**
+     * Get the list of claims requested by a client application.
+     *
+     * <p>
+     * A <i>claim</i> is a piece of information about an end-user.
+     * Some standard claim names such as {@code given_name} and
+     * {@code email} are defined in "<a href=
+     * "http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims"
+     * >5.1. Standard Claims</a>" in <a href=
+     * ""http://openid.net/specs/openid-connect-core-1_0.html">OpenID
+     * Connect Core 1.0</a>. A service implementation should extract
+     * data corresponding to the claims from its database and return
+     * them to Authlete. The data will be embedded in an ID token.
+     * </p>
+     *
+     * <p>
+     * Note that a claim name may be followed by <code>#<i>locale</i></code>.
+     * For example, <code>family_name#ja</code>. See "<a href=
+     * "http://openid.net/specs/openid-connect-core-1_0.html#ClaimsLanguagesAndScripts"
+     * >5.2. Claims Languages and Scripts</a>" in <a href=
+     * "http://openid.net/specs/openid-connect-core-1_0.html">OpenID
+     * Connect Core 1.0</a> for details.
+     * </p>
+     *
+     * <p>
+     * This property is null when claim data are not necessary
+     * (= when an ID token is not necessary to be generated).
+     * </p>
+     *
+     * @return
+     *         The list of claims requested by a client application.
+     */
+    public String[] getClaims()
+    {
+        return claims;
+    }
+
+
+    /**
+     * Set the list of claims requested by a client application.
+     *
+     * @param claims
+     *         The list of claims requested by a client application.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackRequest setClaims(String[] claims)
+    {
+        this.claims = claims;
+
+        return this;
+    }
+
+
+    /**
+     * Get the list of locales for claims.
+     *
+     * <p>
+     * This property holds the value of {@code claims_locales} request
+     * parameter contained in an authorization request. The values are
+     * the end-user's preferred languages and scripts for claims. See
+     * "<a href="http://openid.net/specs/openid-connect-core-1_0.html#ClaimsLanguagesAndScripts"
+     * >5.2. Claims Languages and Scripts</a>" in <a href=
+     * "http://openid.net/specs/openid-connect-core-1_0.html">OpenID
+     * Connect Core 1.0</a> for details.
+     * </p>
+     *
+     * <p>
+     * This property is null when claim data are not necessary
+     * (= when an ID token is not necessary to be generated).
+     * </p>
+     *
+     * @return
+     *         The list of locales for claims.
+     */
+    public String[] getClaimsLocales()
+    {
+        return claimsLocales;
+    }
+
+
+    /**
+     * Set the list of locales for claims.
+     *
+     * @param claimsLocales
+     *         The list of locales for claims.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackRequest setClaimsLocales(String[] claimsLocales)
+    {
+        this.claimsLocales = claimsLocales;
+
+        return this;
+    }
+}
---
+++ src/main/java/com/authlete/common/dto/AuthenticationCallbackResponse.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (C) 2014 Authlete, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.authlete.common.dto;
+
+
+import java.io.Serializable;
+
+
+/**
+ * Authentication response from a service implementation to Authlete.
+ *
+ * @author Takahiko Kawasaki
+ */
+public class AuthenticationCallbackResponse implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+
+    private boolean authenticated;
+    private String subject;
+    private String claims;
+
+
+    /**
+     * Get the authentication result.
+     *
+     * @return
+     *         {@code true} if the credentials of the end-user were valid.
+     *         Otherwise, {@code false}.
+     */
+    public boolean isAuthenticated()
+    {
+        return authenticated;
+    }
+
+
+    /**
+     * Set the authentication result.
+     *
+     * <p>
+     * When the credentials ({@code id} and {@code password}) in the
+     * authentication callback request ({@link AuthenticationCallbackRequest})
+     * are valid, {@code true} should be set to this property.
+     * </p>
+     *
+     * @param authenticated
+     *         {@code true} if the credentials of the end-user were valid.
+     *         Otherwise, {@code false}.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackResponse setAuthenticated(boolean authenticated)
+    {
+        this.authenticated = authenticated;
+
+        return this;
+    }
+
+
+    /**
+     * Get the subject (= unique identifier) of the authenticated user.
+     *
+     * @return
+     *         The subject (= unique identifier) of the authenticated user.
+     */
+    public String getSubject()
+    {
+        return subject;
+    }
+
+
+    /**
+     * Set the subject (= unique identifier) of the authenticated user.
+     *
+     * <p>
+     * When the credentials ({@code id} and {@code password}) in the
+     * authentication callback request ({@link AuthenticationCallbackRequest})
+     * are valid, the subject (= unique identifier) of the end-user should
+     * be set to this property.
+     * </p>
+     *
+     * <p>
+     * The value of {@code subject} does not always have to be equal to
+     * the value of {@code id} in the authentication callback request.
+     * For example, {@code id} may be an email address but a service
+     * implementation may have generated and assigned a unique identifier
+     * such as {@code 60504791} to the end-user who is represented by
+     * the email address. In such a case, {@code 60504791} should be set
+     * as {@code subject}.
+     * </p>
+     *
+     * @param subject
+     *         The subject (= unique identifier) of the authenticated user.
+     *         When the authentication failed, this property should be
+     *         {@code null}.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackResponse setSubject(String subject)
+    {
+        this.subject = subject;
+
+        return this;
+    }
+
+
+    /**
+     * Get the claims of the authenticated user in JSON format.
+     *
+     * @return
+     *         The claims of the authenticated user in JSON format.
+     */
+    public String getClaims()
+    {
+        return claims;
+    }
+
+
+    /**
+     * Set the claims of the authenticated user in JSON format.
+     *
+     * <p>
+     * For example, to embed "<code>given_name</code>" claim,
+     * "<code>family_name</code>" claim and "<code>email</code>"
+     * claim, the string should be formatted like the following.
+     * </p>
+     *
+     * <pre>
+     * {
+     *   "given_name": "Takahiko",
+     *   "family_name": "Kawasaki",
+     *   "email": "takahiko.kawasaki@example.com"
+     * }
+     * </pre>
+     *
+     * <p>
+     * See "<a href=
+     * "http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims"
+     * >5.1. Standard Claims</a>" in <a href=
+     * "http://openid.net/specs/openid-connect-core-1_0.html"
+     * >OpenID Connect Core 1.0</a> for further details about
+     * the format.
+     * </p>
+     *
+     * <p>
+     * This property does not have to be set (1) when the credentials
+     * ({@code id} and {@code password}) in the authentication callback
+     * request ({@link AuthenticationCallbackRequest}) were invalid,
+     * (2) when the authentication callback request did not contain
+     * any claims (= {@code claims} request parameter was {@code null}
+     * or empty), or (3) when the service implementation could not
+     * provide data for any of the requested claims.
+     * </p>
+     *
+     * @param claims
+     *         The claims of the authenticated user in JSON format.
+     *
+     * @return
+     *         {@code this} object.
+     */
+    public AuthenticationCallbackResponse setClaims(String claims)
+    {
+        this.claims = claims;
+
+        return this;
+    }
+}
