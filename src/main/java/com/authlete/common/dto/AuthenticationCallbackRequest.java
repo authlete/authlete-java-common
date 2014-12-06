39a23e007ee303716a035ed326360592fc850940
Takahiko Kawasaki <taka@authlete.com>
2014-12-06 13:24:27 +0900

    Updated JavaDoc of AuthentcationCallbackRequest.

--- src/main/java/com/authlete/common/dto/AuthenticationCallbackRequest.java
+++ src/main/java/com/authlete/common/dto/AuthenticationCallbackRequest.java
@@ -23,6 +23,70 @@ import com.authlete.common.types.Sns;
 /**
  * Authentication request from Authlete to a service implementation.
  *
+ * <p>
+ * After a client application accesses the authorization endpoint which
+ * Authlete provides for the service
+ * (<code>/api/auth/authorization/direct/<i>{service-api-key}</i></code>)
+ * and an end-user of the service tries end-user authentication at the UI
+ * displayed at the authorization endpoint (by inputting his/her login ID
+ * and password to the input fields or by signing in an SNS such as
+ * Facebook), Authlete makes an <strong>authentication callback request
+ * </strong> to the authentication callback endpoint of the service.
+ * This class represents the format of the request.
+ * </p>
+ *
+ * <p>
+ * When the end-user tried end-user authentication by inputting his/her
+ * credentials to the input fields of the form, {@code id} and {@code
+ * password} in this authentication callback request are the values
+ * that the end-user has input.
+ * </p>
+ *
+ * <p>
+ * On the other hand, when the end-user tried end-user authentication by
+ * signing in an SNS such as Facebook, {@code id} represents the subject
+ * (unique identifier) of the end-user in the SNS and {@code password}
+ * has no meaning. In this case, {@code sns} and {@code accessToken} are
+ * not {@code null}. {@code accessToken} in this request is the value of
+ * the access token issued by the SNS which an implementation of an
+ * authentication callback endpoint may use as necessary.
+ * </p>
+ *
+ * <p>
+ * Some notes specific to respective SNSes.
+ * </p>
+ *
+ * <blockquote>
+ * <dl>
+ *   <dt><strong>Facebook</strong></dt>
+ *   <dd>
+ *     <p>
+ *       The value of <strong>{@code id}</strong> is unique to each
+ *       Facebook application and cannot be used across different
+ *       applications. If you need the third party ID, make an API
+ *       call to <code>/me</code> endpoint with
+ *       <code>fields=third_party_id</code> and
+ *       <code>access_token=<i>{accessToken}</i></code>. See the API
+ *       document of Facebook for details.
+ *     </p>
+ *     <p>
+ *       The value of <strong>{@code rawTokenResponse}</strong> is
+ *       in the form of <code>application/x-www-form-urlencoded</code>
+ *       (not <code>application/json</code>). This is a violation
+ *       against RFC 6749 (OAuth 2.0).
+ *     </p>
+ *     <p>
+ *       The value of <strong>{@code refreshToken}</strong> is empty.
+ *     </p>
+ *     <p>
+ *       The value of <strong>expiresIn</strong> is the value of
+ *       <code>expires</code> in the response from the token endpoint
+ *       of Facebook.
+ *     </p>
+ *   </dd>
+ * </dl>
+ * </blockquote>
+ *
  * @author Takahiko Kawasaki
  *
  * @since 1.1
@@ -122,11 +186,13 @@ public class AuthenticationCallbackRequest implements Serializable
      * this property holds the value of the login ID that the end-user
      * has entered to the login ID field in the UI of the <a href=
      * "https://tools.ietf.org/html/rfc6749#section-3.1">authorization
-     * endpoint</a>, or the value of {@code username} request parameter
-     * to the <a href="https://tools.ietf.org/html/rfc6749#section-3.2"
-     * >token endpoint</a> in the case of <a href=
-     * "https://tools.ietf.org/html/rfc6749#section-4.3">Resource
-     * Owner Password Credentials</a> flow.
+     * endpoint</a>
+     * (<code>/api/auth/authorization/direct/<i>{service-api-key}</i></code>),
+     * or the value of {@code username} request parameter to the <a href=
+     * "https://tools.ietf.org/html/rfc6749#section-3.2">token endpoint</a>
+     * (<code>/api/auth/token/direct/<i>{service-api-key}</i></code>) in the
+     * case of <a href="https://tools.ietf.org/html/rfc6749#section-4.3"
+     * >Resource Owner Password Credentials</a> flow.
      * </p>
      *
      * <p>
@@ -172,11 +238,13 @@ public class AuthenticationCallbackRequest implements Serializable
      * This property holds the value of the password that the end-user
      * has entered to the password field in the UI of the <a href=
      * "https://tools.ietf.org/html/rfc6749#section-3.1">authorization
-     * endpoint</a>, or the value of {@code password} request parameter
-     * to the <a href="https://tools.ietf.org/html/rfc6749#section-3.2"
-     * >token endpoint</a> in the case of <a href=
-     * "https://tools.ietf.org/html/rfc6749#section-4.3">Resource
-     * Owner Password Credentials</a> flow.
+     * endpoint</a>
+     * (<code>/api/auth/authorization/direct/<i>{service-api-key}</i></code>),
+     * or the value of {@code password} request parameter to the <a href=
+     * "https://tools.ietf.org/html/rfc6749#section-3.2">token endpoint</a>
+     * (<code>/api/auth/token/direct/<i>{service-api-key}</i></code>) in the
+     * case of <a href="https://tools.ietf.org/html/rfc6749#section-4.3"
+     * >Resource Owner Password Credentials</a> flow.
      * </p>
      *
      * <p>
@@ -223,7 +291,7 @@ public class AuthenticationCallbackRequest implements Serializable
      * {@code email} are defined in "<a href=
      * "http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims"
      * >5.1. Standard Claims</a>" in <a href=
-     * ""http://openid.net/specs/openid-connect-core-1_0.html">OpenID
+     * "http://openid.net/specs/openid-connect-core-1_0.html">OpenID
      * Connect Core 1.0</a>. A service implementation should extract
      * data corresponding to the claims from its database and return
      * them to Authlete. The data will be embedded in an ID token.
