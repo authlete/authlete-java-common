d01ebe3613df43f8059c3b767c72ba0b0884bd43
Takahiko Kawasaki <taka@authlete.com>
2015-08-22 15:43:33 +0900

    Updated documents for version 1.12.

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Maven
 <dependency>
     <groupId>com.authlete</groupId>
     <artifactId>authlete-java-common</artifactId>
-    <version>1.10</version>
+    <version>1.12</version>
 </dependency>
 ```
 
--- src/main/java/com/authlete/common/package-info.java
+++ src/main/java/com/authlete/common/package-info.java
@@ -17,11 +17,11 @@
  * <span class="tag">&lt;dependency&gt;
  *     &lt;groupId&gt;</span>com.authlete<span class="tag">&lt;/groupId&gt;
  *     &lt;artifactId&gt;</span>authlete-java-common<span class="tag">&lt;/artifactId&gt;
- *     &lt;version&gt;</span>1.10<span class="tag">&lt;/version&gt;
+ *     &lt;version&gt;</span>1.12<span class="tag">&lt;/version&gt;
  * &lt;/dependency&gt;</span><!--
  * --></pre>
  *
- * @version 1.10
+ * @version 1.12
  *
  * @author Takahiko Kawasaki
  */
