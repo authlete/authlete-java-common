289bfb6adb9f71d24d0c2fe23d59e043408046de
Takahiko Kawasaki <taka@authlete.com>
2016-01-09 22:39:40 +0900

    Updated documents for version 1.25.

--- src/main/java/com/authlete/common/package-info.java
+++ src/main/java/com/authlete/common/package-info.java
@@ -17,11 +17,11 @@
  * <span class="tag">&lt;dependency&gt;
  *     &lt;groupId&gt;</span>com.authlete<span class="tag">&lt;/groupId&gt;
  *     &lt;artifactId&gt;</span>authlete-java-common<span class="tag">&lt;/artifactId&gt;
- *     &lt;version&gt;</span>1.24<span class="tag">&lt;/version&gt;
+ *     &lt;version&gt;</span>1.25<span class="tag">&lt;/version&gt;
  * &lt;/dependency&gt;</span><!--
  * --></pre>
  *
- * @version 1.24
+ * @version 1.25
  *
  * @author Takahiko Kawasaki
  */
