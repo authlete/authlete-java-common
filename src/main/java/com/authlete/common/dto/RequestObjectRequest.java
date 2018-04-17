0cc9a300b700686bb72f04fe043befeb8255c866
Justin Richer <justin.richer@authlete.com>
2018-04-17 17:04:24 -0400

    added shell for request object request

---
+++ src/main/java/com/authlete/common/dto/RequestObjectRequest.java
@@ -0,0 +1,17 @@
+package com.authlete.common.dto;
+
+import java.io.Serializable;
+
+/**
+ * Request to Authlete's /auth/requestobject API.
+ * 
+ * @author jricher
+ *
+ */
+public class RequestObjectRequest implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    
+    
+}
