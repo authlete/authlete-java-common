452aeee3aa064a3abd0682aa4cf875e774ea9f40
Justin Richer <justin.richer@authlete.com>
2018-04-18 17:44:56 -0400

    added body to request

--- src/main/java/com/authlete/common/dto/RequestObjectRequest.java
+++ src/main/java/com/authlete/common/dto/RequestObjectRequest.java
@@ -12,6 +12,46 @@ public class RequestObjectRequest implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
-    
+    /**
+     * FAPI Request object endpoint request body (in JWS format).
+     */
+    private String body;
+
+    /**
+     * Get the value of the HTTP message body sent to the
+     * request object endpoint. This consists entirely of a
+     * JSON Web Token (JWT) signed with Json Web Signatures
+     * (JWS) representing the client's request object.
+     * 
+     * @return
+     *          The body string.
+     * 
+     * @since 2.XX
+     */
+    public String getBody()
+    {
+        return body;
+    }
+
+    /**
+     * Get the value of the HTTP message body sent to the
+     * request object endpoint. This consists entirely of a
+     * JSON Web Token (JWT) signed with Json Web Signatures
+     * (JWS) representing the client's request object.
+     * 
+     * @param body
+     *          The body string.
+     *          
+     * @return
+     *          this object
+     *          
+     * @since 2.XX
+     */
+    public RequestObjectRequest setBody(String body)
+    {
+        this.body = body;
+        
+        return this;
+    }
     
 }
