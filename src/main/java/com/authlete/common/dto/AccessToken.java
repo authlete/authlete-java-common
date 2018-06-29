f58aa8e0bb177002f889bef0012e6f7a10578248
Hideki Ikeda <hidebike712@gmail.com>
2018-06-29 17:47:51 +0900

    Fixed descriptions for the getter and setter for the 'subject' field in AccessToken.

--- src/main/java/com/authlete/common/dto/AccessToken.java
+++ src/main/java/com/authlete/common/dto/AccessToken.java
@@ -132,13 +132,13 @@ public class AccessToken implements Serializable
     /**
      * Get the subject (= unique user ID) associated with the access token.
      * {@code null} is returned if the access token was created using the
-     * <a href="https://tools.ietf.org/html/rfc6749#section-1.3.4"> Client Credentials</a>
+     * <a href="https://tools.ietf.org/html/rfc6749#section-1.3.4">Client Credentials</a>
      * flow.
      *
      * @return
      *         The subject (= unique user ID) associated with the access token or
      *         {@code null} if the access token was created using the
-     *         <a href="https://tools.ietf.org/html/rfc6749#section-1.3.4"> Client Credentials</a>
+     *         <a href="https://tools.ietf.org/html/rfc6749#section-1.3.4">Client Credentials</a>
      *         flow.
      */
     public String getSubject()
