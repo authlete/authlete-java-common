f14e399d5adb7ed5a9526ef5e62907209c425611
Takahiko Kawasaki <taka@authlete.com>
2018-09-22 08:55:42 +0900

    Added new entries and methods to ResponseMode to support JARM.

--- src/main/java/com/authlete/common/types/ResponseMode.java
+++ src/main/java/com/authlete/common/types/ResponseMode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2015 Authlete, Inc.
+ * Copyright (C) 2014-2018 Authlete, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,9 +23,9 @@ import java.util.EnumSet;
  * Values for {@code response_mode}.
  *
  * <p>
- * From <a href="http://openid.net/specs/oauth-v2-multiple-response-types-1_0.html"
+ * From <a href="https://openid.net/specs/oauth-v2-multiple-response-types-1_0.html"
  * >OAuth 2.0 Multiple Response Type Encoding Practices</a>,
- * <a href="http://openid.net/specs/oauth-v2-multiple-response-types-1_0.html#ResponseModes"
+ * <a href="https://openid.net/specs/oauth-v2-multiple-response-types-1_0.html#ResponseModes"
  * >2.1. Response Modes</a>
  * </p>
  * <blockquote>
@@ -49,7 +49,7 @@ import java.util.EnumSet;
  * <br/>
  *
  * <p>
- * From <a href="http://openid.net/specs/oauth-v2-form-post-response-mode-1_0.html"
+ * From <a href="https://openid.net/specs/oauth-v2-form-post-response-mode-1_0.html"
  * >OAuth 2.0 Form Post Response Mode</a>
  * </p>
  * <blockquote>
@@ -75,8 +75,43 @@ import java.util.EnumSet;
  * </blockquote>
  * <br/>
  *
- * @see <a href="http://openid.net/specs/oauth-v2-form-post-response-mode-1_0.html"
+ * <p>
+ * From <a href="https://openid.net/specs/openid-financial-api-jarm.html"
+ * >Financial-grade API: JWT Secured Authorization Response Mode for OAuth 2.0 (JARM)</a>
+ * </p>
+ * <blockquote>
+ * <dl>
+ * <dt>{@code jwt}</dt>
+ * <dd>
+ * In this mode, Authorization Response parameters are packed into a JWT
+ * and the resultant JWT is embedded in the default place (the query part
+ * of the redirect URI, the fragment part of the redirect URI, or the HTML
+ * form in the response body).
+ * </dd>
+ * <dt>{@code query.jwt}</dt>
+ * <dd>
+ * In this mode, Authorization Response parameters are packed into a JWT
+ * and the resultant JWT is embedded in the query part of the redirect URI.
+ * </dd>
+ * <dt>{@code fragment.jwt}</dt>
+ * <dd>
+ * In this mode, Authorization Response parameters are packed into a JWT
+ * and the resultant JWT is embedded in the fragment part of the redirect URI.
+ * </dd>
+ * <dt>{@code form_post.jwt}</dt>
+ * <dd>
+ * In this mode, Authorization Response parameters are packed into a JWT
+ * and the resultant JWT is embedded in the HTML form in the response body.
+ * </dd>
+ * </dl>
+ * </blockquote>
+ * <br/>
+ *
+ * @see <a href="https://openid.net/specs/oauth-v2-form-post-response-mode-1_0.html"
  *      >OAuth 2.0 Form Post Response Mode</a>
+ *
+ * @see <a href="https://openid.net/specs/openid-financial-api-jarm.html"
+ *      >Financial-grade API: JWT Secured Authorization Response Mode for OAuth 2.0 (JARM)</a>
  *
  * @author Takahiko Kawasaki
  */
@@ -100,7 +135,43 @@ public enum ResponseMode
      * {@code "form_post"} (3); {@code response_mode} to request authorization
      * response parameters be encoded as HTML form values.
      */
-    FORM_POST((short)3, "form_post")
+    FORM_POST((short)3, "form_post"),
+
+    /**
+     * {@code "jwt"} (4); {@code response_mode} to request authorization response
+     * parameters be encoded as JWT and embedded in the default part. This is a
+     * shortcut of <code>"<i>{default}</i>.jwt"</code>.
+     *
+     * @since 2.27
+     */
+    JWT((short)4, "jwt"),
+
+    /**
+     * {@code "query.jwt"} (5); {@code response_mode} to request authorization
+     * response parameters be encoded as JWT and embedded in the query part of
+     * the redirect URI.
+     *
+     * @since 2.27
+     */
+    QUERY_JWT((short)5, "query.jwt"),
+
+    /**
+     * {@code "fragment.jwt"} (6); {@code response_mode} to request authorization
+     * response parameters be encoded as JWT and embedded in the fragment part of
+     * the redirect URI.
+     *
+     * @since 2.27
+     */
+    FRAGMENT_JWT((short)6, "fragment.jwt"),
+
+    /**
+     * {@code "form_post.jwt"} (7); {@code response_mode} to request authorization
+     * response parameters be encoded as JWT and embedded in the HTML form in the
+     * response body.
+     *
+     * @since 2.27
+     */
+    FORM_POST_JWT((short)7, "form_post.jwt"),
     ;
 
 
@@ -151,6 +222,71 @@ public enum ResponseMode
         }
 
         return sValues[value - 1];
+    }
+
+
+    /**
+     * Check whether this response mode requires response parameters be
+     * packed into a JWT.
+     *
+     * @return
+     *         {@code true} if this response mode is one of {@link #JWT},
+     *         {@link #QUERY_JWT}, {@link #FRAGMENT_JWT} and
+     *         {@link #FORM_POST_JWT}.
+     *
+     * @since 2.27
+     */
+    public boolean isJwtRequired()
+    {
+        return (mValue & JWT.mValue) != 0;
+    }
+
+
+    /**
+     * Check whether this response mode requires response parameters be
+     * embedded in the query part of the redirect URI.
+     *
+     * @return
+     *         {@code true} if this response mode is either {@link #QUERY} or
+     *         {@link #QUERY_JWT}. Otherwise, {@code false}.
+     *
+     * @since 2.27
+     */
+    public boolean isQueryRequired()
+    {
+        return (mValue & 0x3) == QUERY.mValue;
+    }
+
+
+    /**
+     * Check whether this response mode requires response parameters be
+     * embedded in the fragment part of the redirect URI.
+     *
+     * @return
+     *         {@code true} if this response mode is either {@link #FRAGMENT} or
+     *         {@link #FRAGMENT_JWT}. Otherwise, {@code false}.
+     *
+     * @since 2.27
+     */
+    public boolean isFragmentRequired()
+    {
+        return (mValue & 0x3) == FRAGMENT.mValue;
+    }
+
+
+    /**
+     * Check whether this response mode requires response parameters be
+     * embedded in the HTML form in the response body.
+     *
+     * @return
+     *         {@code true} if this response mode is either {@link #FORM_POST}
+     *         or {@link #FORM_POST_JWT}.
+     *
+     * @since 2.27
+     */
+    public boolean isFormPostRequired()
+    {
+        return (mValue & 0x3) == FORM_POST.mValue;
     }
 
 
