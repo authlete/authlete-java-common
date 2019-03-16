4ccbcbedeef8fd03b82b4c5820cd4590a14d9cd6
Takahiko Kawasaki <taka@authlete.com>
2019-03-16 18:21:56 +0900

    Added clientCertificate property to UserInfoRequest class.

--- src/main/java/com/authlete/common/dto/UserInfoRequest.java
+++ src/main/java/com/authlete/common/dto/UserInfoRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Authlete, Inc.
+ * Copyright (C) 2015-2019 Authlete, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,6 +47,15 @@ public class UserInfoRequest implements Serializable
 
 
     /**
+     * The client certificate used in the TLS connection established
+     * between the client application and the userinfo endpoint.
+     *
+     * @since 2.37
+     */
+    private String clientCertificate;
+
+
+    /**
      * Get the access token which has come along with the userinfo
      * request from the client application.
      */
@@ -65,6 +74,49 @@ public class UserInfoRequest implements Serializable
     public UserInfoRequest setToken(String token)
     {
         this.token = token;
+
+        return this;
+    }
+
+
+    /**
+     * Get the client certificate used in the TLS connection established
+     * between the client application and the userinfo endpoint.
+     *
+     * @return
+     *         The client certificate in PEM format.
+     */
+    public String getClientCertificate()
+    {
+        return clientCertificate;
+    }
+
+
+    /**
+     * Set the client certificate used in the TLS connection established
+     * between the client application and the userinfo endpoint.
+     *
+     * <p>
+     * The value of this request parameter is referred to when the access
+     * token given to the userinfo endpoint was bound to a client certificate
+     * when it was issued. See <i>"<a href=
+     * "https://datatracker.ietf.org/doc/draft-ietf-oauth-mtls/?include_text=1"
+     * >OAuth 2.0 Mutual TLS Client Authentication and Certificate-Bound Access
+     * Tokens</a>"</i> for details about the specification of certificate-bound
+     * access tokens.
+     * </p>
+     *
+     * @param certificate
+     *         The client certificate in PEM format.
+     *
+     * @return
+     *         {@code this} object.
+     *
+     * @since 2.37
+     */
+    public UserInfoRequest setClientCertificate(String certificate)
+    {
+        this.clientCertificate = certificate;
 
         return this;
     }
