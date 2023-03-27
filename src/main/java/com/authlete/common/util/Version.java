adbe06fdf6405282c7f30e66fd064f00e7539512
Maxime Longuepe <maxime.longuepe@authlete.com>
2023-03-27 17:19:32 +0900

    Added compareTo method

--- src/main/java/com/authlete/common/util/Version.java
+++ src/main/java/com/authlete/common/util/Version.java
@@ -1,53 +1,82 @@
 package com.authlete.common.util;
 
+
 import java.util.Objects;
 
-public class Version implements Comparable<Version>{
+
+public class Version implements Comparable<Version>
+{
 
     private int major;
     private int minor;
     private int patch;
 
 
-    public Version(int major, int minor, int patch) {
+    public Version(int major, int minor, int patch)
+    {
         this.major = major;
         this.minor = minor;
         this.patch = patch;
     }
 
-    public int getMajor() {
+
+    public int getMajor()
+    {
         return major;
     }
 
-    public int getMinor() {
+
+    public int getMinor()
+    {
         return minor;
     }
 
-    public int getPatch() {
+
+    public int getPatch()
+    {
         return patch;
     }
 
+
     @Override
-    public String toString() {
+    public String toString()
+    {
         return major + "." + minor + "." + patch;
     }
 
+
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Version version = (Version) o;
         return major == version.major && minor == version.minor && patch == version.patch;
     }
 
+
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         return Objects.hash(major, minor, patch);
     }
 
 
     @Override
-    public int compareTo(Version version) {
-        return 0;
+    public int compareTo(Version version)
+    {
+        if (this.major != version.major)
+        {
+            return Integer.compare(this.major, version.major);
+        }
+
+        if (this.minor != version.minor)
+        {
+            return Integer.compare(this.minor, version.minor);
+        }
+
+        return Integer.compare(this.patch, version.patch);
     }
 }
