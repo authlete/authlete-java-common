9fdcc07892aef36801fa69b2ed9b314602dc722e
Hideki Ikeda <hide@authlete.com>
2024-04-18 23:35:50 +0900

    Update serialVersionUID of Service.

--- src/main/java/com/authlete/common/dto/Service.java
+++ src/main/java/com/authlete/common/dto/Service.java
@@ -330,7 +330,7 @@ import com.authlete.common.types.UserCodeCharset;
  */
 public class Service implements Serializable
 {
-    private static final long serialVersionUID = 80L;
+    private static final long serialVersionUID = 81L;
 
 
     /*
