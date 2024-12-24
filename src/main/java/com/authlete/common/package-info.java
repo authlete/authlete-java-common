e34ed34e58d529763b4af7ab88b100f86a2a628d
Hideki Ikeda <hide@authlete.com>
2024-12-24 18:30:57 +0900

    Updated documents for version 4.16.

--- src/main/java/com/authlete/common/package-info.java
+++ src/main/java/com/authlete/common/package-info.java
@@ -17,11 +17,11 @@
  * <span class="tag">&lt;dependency&gt;
  *     &lt;groupId&gt;</span>com.authlete<span class="tag">&lt;/groupId&gt;
  *     &lt;artifactId&gt;</span>authlete-java-common<span class="tag">&lt;/artifactId&gt;
- *     &lt;version&gt;</span>4.15<span class="tag">&lt;/version&gt;
+ *     &lt;version&gt;</span>4.16<span class="tag">&lt;/version&gt;
  * &lt;/dependency&gt;</span><!--
  * --></pre>
  *
- * @version 4.15
+ * @version 4.16
  */
 package com.authlete.common;
 
